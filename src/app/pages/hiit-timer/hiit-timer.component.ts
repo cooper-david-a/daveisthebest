cd05e8a3918f89ac75b834e4a6d0371739f8da1c
Dave <cooper.david.a@protonmail.com>
2023-11-25 11:36:13 -0600

    hiit timer styled, need open dialog/overlay

--- src/app/pages/hiit-timer/hiit-timer.component.ts
+++ src/app/pages/hiit-timer/hiit-timer.component.ts
@@ -6,16 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./hiit-timer.component.scss'],
 })
 export class HiitTimerComponent {
+  readyToSave: boolean = false;
   running: boolean = false;
   rows: Row[] = [];
   displayedColumns: string[] = ['hard', 'easy', 'rounds'];
+  roundTime: number = 0;
+  elapsedTime: number = 0;
+  remainingTime: number = 0;
 
   playPause() {}
   stop() {}
-  addRow(){}
-  deleteRow(){}
-  openTimer(){}
-  saveTimer(){}
+  addRow() {}
+  deleteRow() {}
+  openTimer() {}
+  saveTimer() {}
 }
 
 interface Row {
