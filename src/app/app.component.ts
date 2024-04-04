09f2ef488db00f353fab64214c723d032769ca31
Dave <cooper.david.a@protonmail.com>
2024-04-04 17:03:21 -0500

    everything works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { CommentsComponent } from './comments/comments.component';
 import { AuthService } from './services/auth.service';
+
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    RouterLink,
+    RouterOutlet,
+    CommentsComponent,
+  ],
 })
 export class AppComponent {
   title = 'DaveIsTheBest';
-  constructor(public authService: AuthService){  }
+  constructor(public authService: AuthService) {}
 }
