3cfc24da8e0306b1eb474aff2e326f67eebf0f00
Dave <cooper.david.a@protonmail.com>
2024-05-21 16:47:17 -0500

    thermo-prop-calc still working on it

--- src/app/pages/thermodynamic-property-calculator/thermodynamic-property-calculator.component.ts
+++ src/app/pages/thermodynamic-property-calculator/thermodynamic-property-calculator.component.ts
@@ -18,7 +18,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete'
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+
+declare const Module: {
+  PropsSI: (
+    property: string,
+    input1: string,
+    value1: number,
+    input2: string,
+    value2: number,
+    fluid: string
+  ) => number;
+};
 
 @Component({
   selector: 'thermodynamic-property-calculator',
@@ -33,6 +45,7 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete'
     MatSelectModule,
     MatInputModule,
     MatAutocompleteModule,
+    MatChipsModule,
   ],
 })
 export class ThermodynamicPropertyCalculatorComponent implements OnInit {
@@ -40,6 +53,16 @@ export class ThermodynamicPropertyCalculatorComponent implements OnInit {
   renderer = inject(Renderer2);
   dom = inject(DOCUMENT);
   plotType = 'PH';
+  states: State[] = [];
+
+  get moduleIsLoaded() {
+    if (Module == undefined) {
+      return false;
+    }
+    return !!Module.PropsSI;
+  }
+
+  ready = false;
 
   constructor() {}
 
@@ -51,6 +74,14 @@ export class ThermodynamicPropertyCalculatorComponent implements OnInit {
       coolpropScript.defer = true;
       coolpropScript.type = 'text/javascript';
       this.renderer.appendChild(this.dom.head, coolpropScript);
+      coolpropScript.onload = () => {
+        if (this.moduleIsLoaded) {
+          this.calculate(this.stateForm.controls.statesFormArray.at(0), 0);
+          this.ready = true;
+        } else {
+          setTimeout(coolpropScript.onload, 200);
+        }
+      };
     }
   }
 
@@ -59,14 +90,21 @@ export class ThermodynamicPropertyCalculatorComponent implements OnInit {
       validators: [Validators.required],
       nonNullable: true,
     }),
-    states: new FormArray<FormGroup<StateFormGroup>>([this.stateFactory()]),
+    statesFormArray: new FormArray<FormGroup<StateFormGroup>>([
+      this.stateFormFactory(),
+    ]),
   });
 
   addState() {
-    this.stateForm.controls.states.push(this.stateFactory());
+    let newStateForm = this.stateFormFactory();
+    this.calculate(
+      newStateForm,
+      this.stateForm.controls.statesFormArray.length
+    );
+    this.stateForm.controls.statesFormArray.push(newStateForm);
   }
 
-  stateFactory() {
+  stateFormFactory() {
     return new FormGroup<StateFormGroup>({
       prop1: new FormGroup<FluidPropertyFormGroup>({
         property: new FormControl('P', {
@@ -100,13 +138,170 @@ export class ThermodynamicPropertyCalculatorComponent implements OnInit {
   }
 
   deleteState(index: number) {
-    if (this.stateForm.controls.states.length > 1) {
-      this.stateForm.controls.states.removeAt(index);
-      console.log('index= ', index, 'states= ', this.stateForm.value);
+    if (this.stateForm.controls.statesFormArray.length > 1) {
+      this.stateForm.controls.statesFormArray.removeAt(index);
     }
   }
 
+  calculate(stateFormGroup: FormGroup<StateFormGroup>, index: number) {
+    const fluid = this.stateForm.value.fluid ?? 'Water';
+    const prop1 = <FluidProperty>stateFormGroup.getRawValue().prop1;
+    const prop2 = <FluidProperty>stateFormGroup.getRawValue().prop2;
+    prop1.value =
+      prop1.units && prop1.value
+        ? this.convertToSI(prop1.units, prop1.value)
+        : 0;
+    prop2.value =
+      prop2.units && prop2.value
+        ? this.convertToSI(prop2.units, prop2.value)
+        : 0;
+
+    let state = {
+      Q: Module.PropsSI(
+        'Q',
+        prop1.property,
+        prop1.value,
+        prop2.property,
+        prop2.value,
+        fluid
+      ),
+      T: Module.PropsSI(
+        'T',
+        prop1.property,
+        prop1.value,
+        prop2.property,
+        prop2.value,
+        fluid
+      ),
+      P: Module.PropsSI(
+        'P',
+        prop1.property,
+        prop1.value,
+        prop2.property,
+        prop2.value,
+        fluid
+      ),
+      D: Module.PropsSI(
+        'D',
+        prop1.property,
+        prop1.value,
+        prop2.property,
+        prop2.value,
+        fluid
+      ),
+      U: Module.PropsSI(
+        'U',
+        prop1.property,
+        prop1.value,
+        prop2.property,
+        prop2.value,
+        fluid
+      ),
+      H: Module.PropsSI(
+        'H',
+        prop1.property,
+        prop1.value,
+        prop2.property,
+        prop2.value,
+        fluid
+      ),
+      S: Module.PropsSI(
+        'S',
+        prop1.property,
+        prop1.value,
+        prop2.property,
+        prop2.value,
+        fluid
+      ),
+    };
+    this.states[index] = state;
+  }
+
   propertyPlot() {}
+
+  units: Units = {
+    Q: ['-', '%'],
+    T: ['K', '°C'],
+    P: ['Pa', 'kPa'],
+    D: ['kg/m^3'],
+    U: ['kJ/kg', 'J/kg'],
+    H: ['kJ/kg', 'J/kg'],
+    S: ['kJ/(kg*K)', 'J/(kg*K)'],
+  };
+
+  getPropertyUnits(formGroup: FormGroup<FluidPropertyFormGroup>) {
+    let validUnits =
+      this.units[formGroup.controls.property.value as keyof Units];
+    return validUnits;
+  }
+
+  onPropertyChange(formGroup: FormGroup<FluidPropertyFormGroup>) {
+    let validUnits =
+      this.units[formGroup.controls.property.value as keyof Units];
+    formGroup.controls.units.setValue(validUnits[0]);
+  }
+
+  convertToSI(fromUnits: string, value: number) {
+    switch (fromUnits) {
+      case '-':
+        return value;
+      case '%':
+        return value / 100;
+      case 'K':
+        return value;
+      case '°C':
+        return value + 273.15;
+      case 'Pa':
+        return value;
+      case 'kPa':
+        return value * 1000;
+      case 'MPa':
+        return value * 1e6;
+      case 'kg/m^3':
+        return value;
+      case 'J/kg':
+        return value;
+      case 'kJ/kg':
+        return value * 1000;
+      case 'J/(kg*K)':
+        return value;
+      case 'kJ/(kg*K)':
+        return value * 1000;
+      default:
+        return NaN;
+    }
+  }
+
+  convertFromSI(toUnits: string, value: number) {
+    switch (toUnits) {
+      case '-':
+        return value;
+      case '%':
+        return value * 100;
+      case 'K':
+        return value;
+      case '°C':
+        return value - 273.15;
+      case 'Pa':
+        return value;
+      case 'kPa':
+        return value / 1000;
+      case 'MPa':
+        return value / 1e6;
+      case 'kg/m^3':
+        return value;
+      case 'J/kg':
+        return value;
+      case 'kJ/kg':
+        return value / 1000;
+      case 'J/(kg*K)':
+        return value;
+      case 'kJ/(kg*K)':
+        return value / 1000;
+      default:
+        return NaN;
+    }
+  }
 }
 
 interface StateFormGroup {
@@ -114,8 +309,34 @@ interface StateFormGroup {
   prop2: FormGroup<FluidPropertyFormGroup>;
 }
 
+interface State {
+  Q: number;
+  T: number;
+  P: number;
+  D: number;
+  U: number;
+  H: number;
+  S: number;
+}
+
 interface FluidPropertyFormGroup {
   property: FormControl<string>;
   value: FormControl<number>;
   units: FormControl<string>;
+}
+
+interface FluidProperty {
+  property: string;
+  value: number;
+  units: string;
+}
+
+interface Units {
+  Q: ['-', '%'];
+  T: ['K', '°C'];
+  P: ['Pa', 'kPa'];
+  D: ['kg/m^3'];
+  U: ['kJ/kg', 'J/kg'];
+  H: ['kJ/kg', 'J/kg'];
+  S: ['kJ/(kg*K)', 'J/(kg*K)'];
 }
